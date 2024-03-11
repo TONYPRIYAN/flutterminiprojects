6b0e74a6d66aef192bf28dc6a5527bdc670092b7
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2024-03-11 13:11:01 +0530

    task screen UI changed

--- To_do_list/lib/screens/tasks_screen.dart
+++ To_do_list/lib/screens/tasks_screen.dart
@@ -7,15 +7,56 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Icon(Icons.list),
-            Text('Todo'),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        
+        onPressed: () {
 
-            
-          ],
-        ),
+      },),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60,left: 30,right: 30,bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.lightBlueAccent,
+                      size: 30),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('Todoey',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700),
+                ),
+                Text('Tasks',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
